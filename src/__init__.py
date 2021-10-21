b64ebbbeddf9034f50019855d59a4a9161d91f39
MariaPerera <maria.perera.baro@gmail.com>
2021-10-21 22:23:18 +0200

    Run script

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+import os
+# Posar fitxer d'instalar llibreries
+# Define path with .py codes containing functions used in this script
+#os.getcwd()
+#os.chdir( 'src/features')
+#print(os.getcwd())
+exec(open("src/features/Data_understanding.py").read())
+
+
+
