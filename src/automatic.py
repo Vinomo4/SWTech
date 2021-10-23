00ec8d62b6f1f2105221fb6e6fc7918750fbc10f
MariaPerera <maria.perera.baro@gmail.com>
2021-10-23 19:06:12 +0200

    convert notebook to py Clustering.py

--- src/automatic.py
+++ src/automatic.py
@@ -2,19 +2,20 @@ import os
 
 # Define path with .py codes containing functions used in this script
 
-os.chdir( './features/')
-exec(open("Data_Understanding.py").read())
+#os.chdir( './features/')
+#exec(open("Data_Understanding.py").read())
 
 os.chdir( '../data/')
-exec(open("Data_Preparation.py").read())
+#exec(open("Data_Preparation.py").read())
 
+print()
 os.chdir( '../models/')
-exec(open("Models.py").read())
+exec(open("Clustering.py").read())
 
 #exec(open("Decision Tree Classifier.py").read())
 
-exec(open("Validation.py").read())
+#exec(open("Validation.py").read())
 
 # Check if everything works fine
-os.chdir( '../features/')
-exec(open("Check_tracking.py").read())
+#os.chdir( '../features/')
+#exec(open("Check_tracking.py").read())
