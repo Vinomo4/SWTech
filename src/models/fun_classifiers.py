64d589dc820c4699ed5fd97284621bcc06d14417
Vinomo4 <victornovelle2000@gmail.com>
2021-10-27 12:15:12 +0200

    added accuracy compuation for dt/rf classifiers

--- src/models/fun_classifiers.py
+++ src/models/fun_classifiers.py
@@ -68,7 +68,7 @@ def train_decision_tree(data,part_size,depth = 3):
         - Trained Decision Tree Classifier.
         - Averaged confussion matrix.
     '''
-    cm_list = []
+
     # Training with all the data.
     if not part_size:
         dtc = DecisionTreeClassifier(max_depth = depth, random_state = 0)
@@ -76,6 +76,8 @@ def train_decision_tree(data,part_size,depth = 3):
         dtc.fit(X_train,Y_train)
         return dtc,None,None
     else:
+        cm_list = []
+        acc_list = []
         for i in range(10):
             X_train,X_test, Y_train, Y_test = split_data(data,part_size,i)
             # Creating the decission tree classifer.
@@ -87,6 +89,11 @@ def train_decision_tree(data,part_size,depth = 3):
             # Confussion matrix computation
             cm,labels = generate_confusion_matrix(pred, Y_test)
             cm_list.append(cm)
+            # Computing the accuracy.
+            aux_acc = 0
+            for y_pred,y in zip(pred,Y_test):
+                if y_pred == y: aux_acc += 1
+            acc_list.append(aux_acc/len(pred)*100)
         # An image of the DTC indicating the decision threshold is stored.
         fn=X_train.columns
         cn= sorted(Y_train.unique())
@@ -101,7 +108,7 @@ def train_decision_tree(data,part_size,depth = 3):
         if not os.path.isdir(path):
             os.makedirs(path)
         plt.savefig(path+'Decision_tree.png')
-        return dtc, sum(cm_list)/len(cm_list), labels
+        return dtc, sum(cm_list)/len(cm_list),sum(acc_list)/len(acc_list),labels
 
 def train_random_forest(data,part_size,depth = 3, n_trees = 100):
     '''
@@ -125,21 +132,28 @@ def train_random_forest(data,part_size,depth = 3, n_trees = 100):
         X_train, Y_train = data.drop(labels = "clusters",axis =1), data['clusters']
         rf.fit(X_train,Y_train)
         return rf,None,None
-    cm_list = []
-    for i in range(10):
-        X_train,X_test, Y_train, Y_test = split_data(data,part_size,i)
-        # Creating the random forest.
-        rf = RandomForestClassifier(n_estimators=n_trees,
-                                    max_depth = depth,
-                                    random_state=0)
-        # Training
-        rf.fit(X_train, Y_train)
-        # Prediction
-        pred = rf.predict(X_test)
-        # Confussion matrix computation
-        cm,labels = generate_confusion_matrix(pred, Y_test)
-        cm_list.append(cm)
-    return rf,sum(cm_list)/len(cm_list),labels
+    else:
+        cm_list = []
+        acc_list = []
+        for i in range(10):
+            X_train,X_test, Y_train, Y_test = split_data(data,part_size,i)
+            # Creating the random forest.
+            rf = RandomForestClassifier(n_estimators=n_trees,
+                                        max_depth = depth,
+                                        random_state=0)
+            # Training
+            rf.fit(X_train, Y_train)
+            # Prediction
+            pred = rf.predict(X_test)
+            # Confussion matrix computation
+            cm,labels = generate_confusion_matrix(pred, Y_test)
+            cm_list.append(cm)
+            # Computing the accuracy.
+            aux_acc = 0
+            for y_pred,y in zip(pred,Y_test):
+                if y_pred == y: aux_acc += 1
+            acc_list.append(aux_acc/len(pred)*100)
+        return rf, sum(cm_list)/len(cm_list),sum(acc_list)/len(acc_list),labels
 
 def generate_confusion_heatmap(conf_matrix,labels,method):
     '''
